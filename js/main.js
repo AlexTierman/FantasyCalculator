80040af39d76fe52c890b0b2c39cbf18a666e322
Jordan Veloso <jrveloso96@gmail.com>
2022-12-01 22:09:25 -0600

    Scoring rules auto added to local storage.

--- js/main.js
+++ js/main.js
@@ -17,21 +17,45 @@
 
 // Add buttons below the number input in order to add another form of user input for the league scoring column. Could be beneficial for mobile users.  
 
+//12-1-2022
+//after scoring rules are placed in input -> option to save to local storage
+//input for name of scoring -> saved as key in local storage
+//popup
+
+//IIFE
+(() => {
+  const scoring = localStorage.getItem('league1');
+  console.log(scoring)
+
+  const values = scoring.split(",").map(val => Number(val))
+  console.log(values)
+
+  // grab each element from value array -> loop
+  // display each using textContent
+  let scoringInputs = document.querySelectorAll(".leagueInput")
+
+  scoringInputs.forEach((el, i) => el.value = values[i])
+  
+})()
 
 
 function num(){
   const values = document.querySelectorAll(".leagueInput")
   const scoring = Array.from(values)
-  const leagueScoring = scoring.map(input => Number(input.value))
+  const leagueScoring = scoring.map(input => input.value)
+
+  // console.log(leagueScoring)
+
+  localStorage.setItem("league1", leagueScoring);
+  
 
   const playerValues = document.querySelectorAll(".playerInput")
   const playerInputs = Array.from(playerValues)
   const playerValueArray = playerInputs.map(input => Number(input.value))
-  console.log(leagueScoring, playerValueArray)
+  // console.log(leagueScoring, playerValueArray)
 
   const fantasyPoints = leagueScoring.map((num,i)=> num * playerValueArray[i]).reduce((a,c)=> a + c, 0)
   document.querySelector("span").textContent = fantasyPoints
 }
 
 document.querySelector("#calculate").addEventListener("click", num)
-
